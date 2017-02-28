7f390363c9566ac80d2c07c6715aa8fc0dd9cf5f
Nicholas Lesniak <NLesniak@umich.edu>
2017-02-28 12:06:35 -0500

    add download of mothur

--- analysis_driver.bash
+++ analysis_driver.bash
@@ -7,12 +7,21 @@ tar xvf StabilityWMetaG.tar -C data/raw/
 rm StabilityWMetaG.tar
 
 
+# Download MacOS X version of mothur (v1.39.3) from the mothur GitHub repository
+
+curl -LO https://github.com/mothur/mothur/releases/download/v.1.39.3/Mothur.mac_64.OSX-10.12.zip
+unzip Mothur.mac_64.OSX-10.12.zip
+mv mothur code/
+rm Mothur.mac_64.OSX-10.12.zip
+rm -rf __MACOSX
+
+
 # Download the SILVA reference file (v.123). We will pull out the bacteria-specific sequences and
 # clean up the directories to remove the extra files
 
 curl -LO http://mothur.org/w/images/1/15/Silva.seed_v123.tgz
 tar xvzf Silva.seed_v123.tgz silva.seed_v123.align silva.seed_v123.tax
-mothur "#get.lineage(fasta=silva.seed_v123.align, taxonomy=silva.seed_v123.tax, taxon=Bacteria);degap.seqs(fasta=silva.seed_v123.pick.align, processors=8)"
+code/mothur/mothur "#get.lineage(fasta=silva.seed_v123.align, taxonomy=silva.seed_v123.tax, taxon=Bacteria);degap.seqs(fasta=silva.seed_v123.pick.align, processors=8)"
 mv silva.seed_v123.pick.align data/references/silva.seed.align
 rm Silva.seed_v123.tgz silva.seed_v123.*
 rm mothur.*.logfile
