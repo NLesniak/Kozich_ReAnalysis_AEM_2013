c323b09024a613801e4701fad104d00099cb8b4b
Nicholas Lesniak <NLesniak@umich.edu>
2017-03-01 09:18:12 -0500

    add mothur commands

--- analysis_driver.bash
+++ analysis_driver.bash
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 
 # Download the raw data and put them into the data/raw directory
-
-curl -LO https://www.mothur.org/MiSeqDevelopmentData/StabilityWMetaG.tar
-tar xvf StabilityWMetaG.tar -C data/raw/
-rm StabilityWMetaG.tar
+curl -LO https://www.mothur.org/MiSeqDevelopmentData/StabilityNoMetaG.tar
+tar xvf StabilityNoMetaG.tar -C data/raw/
+rm StabilityNoMetaG.tar
 
 
 # Download MacOS X version of mothur (v1.39.3) from the mothur GitHub repository
-
 curl -LO https://github.com/mothur/mothur/releases/download/v.1.39.3/Mothur.mac_64.OSX-10.12.zip
 unzip Mothur.mac_64.OSX-10.12.zip
 mv mothur code/
@@ -18,7 +16,6 @@ rm -rf __MACOSX
 
 # Download the SILVA reference file (v.123). We will pull out the bacteria-specific sequences and
 # clean up the directories to remove the extra files
-
 curl -LO http://mothur.org/w/images/1/15/Silva.seed_v123.tgz
 tar xvzf Silva.seed_v123.tgz silva.seed_v123.align silva.seed_v123.tax
 code/mothur/mothur "#get.lineage(fasta=silva.seed_v123.align, taxonomy=silva.seed_v123.tax, taxon=Bacteria);degap.seqs(fasta=silva.seed_v123.pick.align, processors=8)"
@@ -34,3 +31,17 @@ tar xvzf Trainset14_032015.pds.tgz trainset14_032015.pds/trainset14_032015.pds.*
 mv trainset14_032015.pds/* data/references/
 rmdir trainset14_032015.pds
 rm Trainset14_032015.pds.tgz
+
+
+# Generate a customized version of the SILVA reference database that targets the V4 region
+code/mothur/mothur "#pcr.seqs(fasta=data/references/silva.seed.align, start=11894, end=25319, keepdots=F, processors=8)"
+mv data/references/silva.seed.pcr.align data/references/silva.v4.align
+
+# process raw sequences
+code/mothur/mothur code/get_good_seqs.batch
+
+# calc sequencing error
+code/mothur/mothur code/get_error.batch
+
+# generate shared files
+code/mothur/mothur code/get_shared_otus.batch
